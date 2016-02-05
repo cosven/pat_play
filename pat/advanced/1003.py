2459c24756d3ac9dfe823f27bca75d7f7694d873
Cosven <yinshaowen241@gmail.com>
2016-02-06 01:43:04 +0800

    add advanced 1003

--- pat/advanced/1003.py
+++ pat/advanced/1003.py
@@ -4,8 +4,17 @@
 
 """
 无向有权图找最短路径。
-"""
 
+代码比想象的多，比较丑。
+
+感知递归的特征.
+
+注意点：
+1. if ... else 判断不要漏
+2. start 和　end 的city可以是一样的
+3. 递归的变量。。。感悟
+"""
+import sys
 from collections import deque
 
 
@@ -31,6 +40,9 @@ for road in roads:
     adjacent_matrix[road[0]][road[1]] =\
         adjacent_matrix[road[1]][road[0]] = road[2]
 
+if city_start_id == city_end_id:
+    print 1, cities_team_amount[city_start_id]
+    sys.exit(0)
 
 # breadth-first-search
 # (distance, last_city)
@@ -45,6 +57,7 @@ def bfs(current_city_id):
         if route_matrix[city_start_id][current_city_id] == city_id:
             continue
 
+        append_flag = True
         tmp_dis = dis + adjacent_matrix[city_start_id][current_city_id]
 
         if adjacent_matrix[city_start_id][city_id] < 0:
@@ -57,50 +70,66 @@ def bfs(current_city_id):
                 route_matrix[city_start_id][city_id] = current_city_id
             elif tmp_dis == adjacent_matrix[city_start_id][city_id]:
                 if type(origin_route) == int:
-                    if origin_route >= 0:
-                        route_matrix[city_start_id][city_id] = [origin_route, current_city_id]
+                    if origin_route >= 0 and origin_route != current_city_id:
+                        route_matrix[city_start_id][city_id] =\
+                                [origin_route, current_city_id]
                     else:
                         route_matrix[city_start_id][city_id] = current_city_id
-                elif type(origin_route) == list:
-                    route_matrix[city_start_id][city_id].append(current_city_id)
+                else:   # list
+                    if current_city_id in route_matrix[city_start_id][city_id]:
+                        pass
+                    else:
+                        route_matrix[city_start_id][city_id].append(
+                            current_city_id)
+            else:
+                append_flag = False
 
         if city_id == city_end_id:
             pass
         else:
-            visited_cities.append(city_id)
+            if append_flag:
+                visited_cities.append(city_id)
+
 
 bfs(city_start_id)
 while len(visited_cities) > 0:
+    # print visited_cities
     city_id = visited_cities.popleft()
     bfs(city_id)
 
-shortest_distance = adjacent_matrix[city_start_id][city_end_id]
-routes = route_matrix[city_start_id][city_end_id]
+# calculate path
+
+# print
+# print
+
+number_route = 0
+total_teams = []
 
 
-def cal_path(tmp_route):
-    path = deque()
-    path.appendleft(city_end_id)
-    path.appendleft(tmp_route)
-    while route_matrix[city_start_id][tmp_route] >= 0:
-        tmp_route = route_matrix[city_start_id][tmp_route]
-        path.appendleft(tmp_route)
-    team_sum_tmp = 0
-    for city in path:
-        team_sum_tmp += cities_team_amount[city]
-    return team_sum_tmp
+def cal_path(current_city, current_teams_num):
+    global number_route, total_teams
+
+    current_teams_num += cities_team_amount[current_city]
+    # print "c:", current_city,
+
+    neighbours = route_matrix[city_start_id][current_city]
+    if type(neighbours) is int:
+        neighbours = [neighbours]
+
+    tmp_num = current_teams_num
+    for neighbour in neighbours:
+        if neighbour == city_start_id:
+            # print 'n:', neighbour,
+            number_route += 1
+            current_teams_num += cities_team_amount[neighbour]
+            total_teams.append(current_teams_num)
+            # print "end"
+        else:
+            # print "next loop:", current_teams_num
+            cal_path(neighbour, tmp_num)
 
 
-if type(routes) == int:
-    team_sum = cal_path(routes)
-    number_route = 1
-else:
-    number_route = len(routes)
-    teams = []
-    for route in routes:
-        tmp_team_sum = cal_path(route)
-        teams.append(tmp_team_sum)
-    teams = sorted(teams, reverse=True)
-    team_sum = teams[0]
-
-print number_route, team_sum
+# print route_matrix
+cal_path(city_end_id, 0)
+total_teams = sorted(total_teams, reverse=True)
+print number_route, total_teams[0]
