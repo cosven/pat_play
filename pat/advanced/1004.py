a895dab92686830839d966197d013211ebfa543d
ysw <yinshaowen241@gmail.com>
2016-03-05 15:34:50 +0800

    1004 部分正确

--- pat/advanced/1004.py
+++ pat/advanced/1004.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+'''
+1. 考虑只有一个根节点的情况
+'''
+
+
 ROOT_ID = '01'
 
 
@@ -11,18 +16,22 @@ def line_input():
 def node_input():
     data = raw_input().split(' ')
     p_node_id = data[0]
-    children_id = data[2:]
-    Node(p_node_id, children_id)
-    for child_id in children_id:
-        Node(child_id, parent_id=p_node_id)
+    children_ids = data[2:]
+    if not Node.exist(p_node_id):
+        Node(p_node_id, children_ids)
+    else:
+        Node.all_nodes[p_node_id].children_ids.extend(children_ids)
+    for child_id in children_ids:
+        if not Node.exist(child_id):
+            Node(child_id, [], parent_id=p_node_id)
 
 
 class Node(object):
     all_nodes = {}
 
-    def __init__(self, nid, children_id=None, parent_id=None):
+    def __init__(self, nid, children_ids=[], parent_id=None):
         self.nid = nid
-        self.children_id = children_id
+        self.children_ids = children_ids
         self.parent_id = parent_id
         Node.all_nodes[nid] = self
 
@@ -34,12 +43,40 @@ class Node(object):
             level += 1
         return level
 
+    @classmethod
+    def exist(cls, nid):
+        if nid in cls.all_nodes:
+            return True
+        return False
+
 
 def main():
+    level_d = {}
+    max_level = 0
     total_nodes, total_non_leaf_nodes = line_input()
+    if total_non_leaf_nodes == 0:
+        print 1
+        return
     for i in xrange(total_non_leaf_nodes):
         node_input()
-    print Node.all_nodes
+
+    for node in Node.all_nodes.values():
+        if node.children_ids == []:
+            level = node.get_level()
+            if level in level_d:
+                level_d[level] += 1
+            else:
+                level_d[level] = 1
+            max_level = level if level > max_level else max_level
+
+    for i in xrange(max_level+1):
+        if i in level_d:
+            if i == max_level:
+                print level_d[i]
+            else:
+                print level_d[i],
+        else:
+            print 0,
 
 
 if __name__ == '__main__':
