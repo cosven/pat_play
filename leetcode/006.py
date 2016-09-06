6c0e2783f4d29c5791cf4612eb85b3414d686659
cosven <yinshaowen241@gmail.com>
2016-09-07 01:38:59 +0800

    add 006 accepted answer

--- leetcode/006.py
+++ leetcode/006.py
@@ -2,34 +2,38 @@ class Solution(object):
     def convert(self, s, num_rows):
         """
         :type s: str
-        :type numRows: int
+        :type num_rows: int
         :rtype: str
         """
         length = len(s)
         if num_rows <= 1 or len(s) <= num_rows:
             return s
-        height = num_rows
-        width = length / (2*height - 2)
-        width_remainder = length % (2*height - 2) - height
-        if width_remainder < 0:
-            width_remainder = 0
-        r_s = ''
-        for i in range(0, height):
-            for j in range(0, width * (height-1) + width_remainder + 1):
-                quotients = j / (height-1)
-                remainders = j % (height-1)
-                if remainders == 0:
-                    index = (2*height-2) * quotients + i
-                    r_s += s[index] if index < len(s) else ''
+        r_s_l = [''] * num_rows
+        for i in xrange(length):
+            q = i / (2 * (num_rows - 1))
+            r = i % (2 * (num_rows - 1))
+
+            if r <= num_rows - 1:
+                x = r
+            else:
+                x = 2 * (num_rows - 1) - r
+
+            if q < 0:
+                y = 0
+            else:
+                y = q * (num_rows - 1)
+                if r <= num_rows - 1:
+                    y += 0
                 else:
-                    if (i+j) % (height-1) == 0:
-                        index = quotients*(2*height-2) + height + remainders - 1
-                        r_s += s[index] if index < len(s) else ''
-        return r_s
+                    y += r - (num_rows - 1)
+            
+            r_s_l[x] += s[i]
+
+        return ''.join(r_s_l)
 
     def test(self):
-        assert self.convert('PAYPALISHIRING', 1) == 'PAYPALISHIRING'
         assert self.convert('abcdefghijk', 4) == 'agbfhceikdj'
+        assert self.convert('PAYPALISHIRING', 1) == 'PAYPALISHIRING'
         assert self.convert('PAYPALISHIRING', 3) == 'PAHNAPLSIIGYIR'
 
 
