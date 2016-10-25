ecef660a775498e51a2d711533ef04c2f79e378c
cosven <yinshaowen241@gmail.com>
2016-10-26 02:33:53 +0800

    update 007, copy from internet

--- leetcode/007.py
+++ leetcode/007.py
@@ -1,9 +1,17 @@
-import os
-
-
 class Solution(object):
     def reverse(self, x):
-        pass
+        if x == 0:
+            return 0
+        if x > ((1 << 31) - 1):
+            return 0
+        flag = -1 if x < 0 else 1
+        x = flag * x
+        while ((x / 10) > 0) and (x % 10 == 0):
+            x = x / 10 
+        if int(str(x)[::-1]) > ((1 << 31) - 1):
+            return 0
+        r_x = flag * int(str(x)[::-1]) 
+        return r_x
 
     def reverse__first_version(self, x):
         """
