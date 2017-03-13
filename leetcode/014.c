a8e785970ab0fad58df7ff539a02ee5847e1c58b
cosven <yinshaowen241@gmail.com>
2017-03-13 09:09:11 +0800

    nothing

---
+++ leetcode/014.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int test_solution();
+
+int main(){
+  test_solution();
+  return 0;
+}
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+}
+
+int test_solution(){
+}
