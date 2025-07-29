bce3cb64c237b7c48e09c4e3129b1633481b68e0
cosven <yinshaowen241@gmail.com>
2025-07-29 21:14:51 +0800

    add 328

--- leetcode/328.py
+++ leetcode/328.py
@@ -10,4 +10,26 @@ class ListNode:
 
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        if head is None or head.next is None:
+            return head
+
+        odd = head
+        even = head.next
+
+        odd_head = ListNode(0, head)
+        even_head = ListNode(0, even)
+
+        while odd is not None and even is not None:
+            odd.next = even.next
+
+            if even.next is not None:
+                odd = even.next
+                even.next = odd.next
+                even = odd.next
+            else:
+                even = None
+
+        odd.next = even_head.next
+        return odd_head.next
+
+
