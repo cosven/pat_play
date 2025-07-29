172720b268f159a34773611ddc66a90b8a1cddd8
cosven <yinshaowen241@gmail.com>
2025-07-29 21:14:31 +0800

    add 328

---
+++ leetcode/328.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
